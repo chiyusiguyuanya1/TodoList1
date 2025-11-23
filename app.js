22304b79d7d99f7abe2ab10a855e18b1e2705186
songqingtong <987653666@qq.com>
2025-11-23 13:52:27 +0800

    第二次提交，搭建了项目整体框架，完成了必须完成的功能（添加/删除/标记已完成未完成/查看事项列表）

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-// app.js
-App({})
+//app.js
+const defaultTime = {
+  defaultWorkTime: 30,
+  defaultRestTime: 5
+}
+
+
+App({
+  onLaunch: function() {
+    let workTime = wx.getStorageSync('workTime')
+    let restTime = wx.getStorageSync('restTime')
+    
+    if (!workTime) {
+      wx.setStorage({
+        key: 'workTime',
+        data: defaultTime.defaultWorkTime
+      })
+    }
+    if (!restTime) {
+      wx.setStorage({
+        key: 'restTime',
+        data: defaultTime.defaultRestTime
+      })
+    }
+  }
+  // viblong: function(){
+
+  //   var vibison = wx.getStorageSync('vibison')
+  //   console.log(vibison)
+  // }
+})
